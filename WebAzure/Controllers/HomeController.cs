b90ae3a3b5de59435468d5489697e2e142b9fab7
Rohit <rr.rohitsharma01@gmail.com>
2021-09-12 10:03:09 +0000

    all

---
+++ WebAzure/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAzure.Models;
+
+namespace WebAzure.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private ITokenServce _tokenServce;
+        public HomeController(ILogger<HomeController> logger, ITokenServce tokenServce )
+        {
+            _logger = logger;
+            _tokenServce = tokenServce;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var view = await _tokenServce.Get();
+
+            IList<string> obj = new List<string>();
+
+            obj.Add(view);
+
+            return View(obj as IEnumerable<string>);
+        }
+ 
+    }
+}
---
+++ WebAzure/TokenService.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Identity.Web;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace WebAzure
+{
+    public interface ITokenServce
+    {
+        public Task<string> Get();
+    }
+    public class TokenService : ITokenServce
+    {
+        private readonly HttpClient _httpClinet;
+        private readonly string _APIScope = string.Empty;
+        private readonly string _APIBaseAddress = string.Empty;
+        private readonly ITokenAcquisition _tokenAcquistion;
+
+        public TokenService(ITokenAcquisition tokenAcquisition , HttpClient httpClient , IConfiguration configuration)
+        {
+            _httpClinet = httpClient;
+            _tokenAcquistion = tokenAcquisition;
+            _APIScope = configuration["APIConfig:APIScope"];
+            _APIBaseAddress = configuration["APIConfig:APIBaseaddress"];
+        }
+
+        public async Task<string> Get()
+        {
+            await FindToke();
+
+            var response = await _httpClinet.GetAsync($"{_APIBaseAddress}/weatherforecast");
+            if(response.StatusCode==System.Net.HttpStatusCode.OK)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var output = JsonConvert.DeserializeObject<string>(content);
+
+                return output;
+            }
+
+            throw new HttpRequestException("Invalid Respinse");
+        }
+
+        private  async Task FindToke()
+        {
+            var accessToken = await _tokenAcquistion.GetAccessTokenForUserAsync(new[] { _APIScope });
+
+            _httpClinet.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            _httpClinet.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
